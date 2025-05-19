e6e6c77850a1e85d5e5d1a40e479e124d927927c
ashutosh_j <ashutosh.j001@gmail.com>
2025-05-19 14:59:13 +0530

    updates

--- training_image2/save_model.py
+++ training_image2/save_model.py
@@ -3,7 +3,5 @@ model_checkpoint = "ashutoshj01/bert-finetuned-squad"
 model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-# saving to local dir
-
-model.save_pretrained("./qna_model")
-tokenizer.save_pretrained("./qna_model")
+model.save_pretrained("./mnt/model")
+tokenizer.save_pretrained("./mnt/model")
