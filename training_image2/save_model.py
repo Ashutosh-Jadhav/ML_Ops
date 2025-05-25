cc5caf321cc20fe83da4c4ef04817b98a26d226d
ashutosh_j <ashutosh.j001@gmail.com>
2025-05-25 18:46:15 +0530

    updates

--- training_image2/save_model.py
+++ training_image2/save_model.py
@@ -1,4 +1,5 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+# model_checkpoint = "ashutoshj01/bert-finetuned-squad"
 model_checkpoint = "ashutoshj01/bert-finetuned-squad"
 model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
